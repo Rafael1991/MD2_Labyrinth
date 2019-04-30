27434459beade995874c5a824a1ecab9f9ac9b36
Rafael1991 <n-fabrizius@web.de>
2019-04-30 19:54:10 +0200

    first try
    kinect runs in DepthMesh-Scene and in RandomMaze Scene runs the Labyrinth

---
+++ Assets/Plugins/Kinect V2/KinectV2.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class KinectV2 : DepthSensorBase
+{   
+    
+    private Windows.Kinect.KinectSensor sensor;
+    private DepthFrameReader reader;
+    private ushort[] data;
+    private int width;
+    private int heigth;
+
+    public override int Width => width;
+
+    public override int Height => heigth;
+
+    public override ushort[] depthImage
+    {
+        get { return data; }
+    }
+
+    public override bool pollDepth()
+    {
+        if (reader != null)
+        {
+            var frame = reader.AcquireLatestFrame();
+            if (frame != null)
+            {
+                frame.CopyFrameDataToArray(data);
+                frame.Dispose();
+                frame = null;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Start()
+    {
+        sensor = Windows.Kinect.KinectSensor.GetDefault();
+
+        if (sensor != null)
+        {
+            reader = sensor.DepthFrameSource.OpenReader();
+            data = new ushort[sensor.DepthFrameSource.FrameDescription.LengthInPixels];
+            width = sensor.DepthFrameSource.FrameDescription.Width;
+            heigth = sensor.DepthFrameSource.FrameDescription.Height;
+
+            if (!sensor.IsOpen)
+                sensor.Open();
+        }
+        else
+        {
+            Debug.LogErrorFormat("Failed to acquire Kinect Sensor!");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (reader != null)
+        {
+            reader.Dispose();
+            reader = null;
+        }
+
+        if (sensor != null)
+        {
+            if (sensor.IsOpen)
+                sensor.Close();
+
+            sensor = null;
+        }
+    }
+  
+}
---
+++ Assets/Script/SpawnObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public int max = 64;
+    public GameObject cube1;
+    [HideInInspector]
+    //public static float Range(float min, float max);
+
+    // Instantiate the Prefab somewhere between -10.0 and 10.0 on the x-z plane
+    void Start() => spawn();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void spawn() 
+    {
+        for (var i = 0; i < max; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+            Instantiate(cube1, position, Quaternion.identity);
+        }
+    }
+}
